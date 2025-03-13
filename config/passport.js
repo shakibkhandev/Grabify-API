3dc0e77e010f161749b90b4b608a9db8054de469
md-shakib-khan <mdshakibkhan.dev@gmail.com>
2025-03-13 15:34:01 +0600

    passport

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const User = require("../models/userModel");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+require("dotenv").config();
 
 
 
